ec744f55fe252a65fd50bd27a733443e65609f47
Dagem587 <dagem1989@gmail.com>
2022-09-17 18:37:15 +0000

    print_numbers addition

---
+++ print_numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+/**
+ * main - prints from 0 - 9
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	int c;
+
+	for (c = 0 ; c <= 9 ; c++)
+	{
+		printf("%d\n", c);
+	}
+	return (0);
+}
